6aca414accbbcab28b80889b7afc55b6992f63f4
yebaital <yousef.baitalmal@gmail.com>
2024-05-06 15:45:28 +0300

    Implement name input functionality in Player component
    The commit introduces a state for "enteredName" and a reference "playerName" to capture the name entered by the user. The name is then displayed on the screen. When the user clicks the "Set Name" button, the entered name is saved and the input field is cleared.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,22 @@
+import {useState, useRef} from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+
+    const playerName = useRef();
+    const [enteredName, setEnteredName] = useState(null);
+
+    function handleClick() {
+        setEnteredName(playerName.current.value);
+        playerName.current.value = '';
+    }
+
+    return (
+        <section id="player">
+            <h2>Welcome {enteredName ?? 'unknown entity'}</h2>
+            <p>
+                <input ref={playerName} type="text"/>
+                <button onClick={handleClick}>Set Name</button>
+            </p>
+        </section>
+    );
 }
